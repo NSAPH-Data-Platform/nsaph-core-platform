f421fdad1eb4de555662fec466c154724b9ca2ad
mmcentre <michael.bouzinier@kvitneset.com>
2020-11-10 03:25:23 -0500

    MAB   Ingesting exposure data

--- src/python/nsaph/csv_loader.py
+++ src/python/nsaph/csv_loader.py
@@ -1,19 +1,49 @@
+from re import Pattern
+
 import psycopg2
 import os
 import re
 import sys
 from configparser import ConfigParser
+import csv
+
+
+def regex(pattern: str) -> Pattern:
+    pattern = 'A' + pattern.replace('.', '_') + 'Z'
+    x = pattern.split('*')
+    y = [re.escape(s) for s in x]
+    regexp = ".*".join(y)[1:-1]
+    return re.compile(regexp)
 
 
 index_columns = [
     "fips",
+    "monitor",
     "name",
-    "year"
+    "year",
+    "zip",
+    regex("*.code"),
+    regex("*.type"),
+    regex("*.name")
 ]
 
 
+NA = "NA"
+
+def is_index_column(c: str) -> bool:
+    c = c.lower()
+    for i in index_columns:
+        if isinstance(i, Pattern):
+            if i.fullmatch(c):
+                return True
+        else:
+            if i == c:
+                return True
+    return False
+
+
 class CSVFileWrapper():
-    def __init__(self, file_like_object, sep = ',', null_replacement = "NA"):
+    def __init__(self, file_like_object, sep = ',', null_replacement = NA):
         self.file_like_object = file_like_object
         self.sep = sep
         self.null_replacement = null_replacement
@@ -69,7 +99,8 @@ class CSVFileWrapper():
 
 
 integer = re.compile("\d+")
-float_number = re.compile("(\d*)\.(\d+)")
+float_number = re.compile("(\d*)\.(\d+)([e|E][-|+]?\d+)?")
+exponent = re.compile("(\d+)([e|E][-|+]?\d+)")
 
 
 def unquote(s: str) -> str:
@@ -122,29 +153,32 @@ def test_connection ():
             print('Database connection closed.')
 
 
-def guess_types(lines: list) -> list:
-    m = len(lines)
-    n = len(lines[0])
+def guess_types(rows: list, lines: list) -> list:
+    m = len(rows)
+    n = len(rows[0])
     types = []
     for c in range(0,n):
         type = None
         precision = 0
         scale = 0
         for l in range(0,m):
-            v = lines[l][c].strip()
-            if '"' in v:
+            v = rows[l][c].strip()
+            v2 = lines[l][c].strip()
+            if '"' in v2:
                 t = "VARCHAR"
-            elif not v or v in ['0', "NA"]:
+            elif not v or v in ['0', NA]:
                 t = "0"
             else:
-                f = float_number.match(v)
+                f = float_number.fullmatch(v)
                 if f:
                     t = "NUMERIC"
                     s = len(f.group(2))
                     p = len(f.group(1))
                     scale = max(scale, s)
                     precision = max(precision, p)
-                elif integer.match(v):
+                elif exponent.fullmatch(v):
+                    t = "NUMERIC"
+                elif integer.fullmatch(v):
                     t = "INT"
                 else:
                     t = "VARCHAR"
@@ -153,6 +187,8 @@ def guess_types(lines: list) -> list:
             if type == "0":
                 type = t
             elif type == "NUMERIC" and t == "INT":
+                continue
+            elif type == "VARCHAR" and t in ["INT", "NUMERIC"]:
                 continue
             elif type == "INT" and t == "NUMERIC":
                 type = t
@@ -170,30 +206,41 @@ def guess_types(lines: list) -> list:
 
 
 def create_table(file_path: str, cursor = None) -> str:
+    has_commas = False
     file_name = os.path.basename(file_path)
     name, _ = os.path.splitext(file_name)
     with open(file_path) as data:
-        line = data.readline()
-        columns = line.split(',')
-        columns = [unquote(c) for c in columns]
+        reader = csv.reader(data, quotechar='"', delimiter=',',
+                     quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        row = next(reader)
+        columns = [unquote(c) for c in row]
 
+        rows = []
+        for i in range(0,10000):
+            rows.append(next(reader))
+    with open(file_path) as data:
+        reader = csv.reader(data, quotechar='"', delimiter=',',
+                     quoting=csv.QUOTE_NONE, skipinitialspace=True)
+        next(reader)
         lines = []
         for i in range(0,10000):
-            line = data.readline()
-            if line:
-                values = line.split(',')
-            else:
-                break
-            lines.append(values)
-        if not lines:
-            raise Exception("No data in {}".format(file_path))
-        types = guess_types(lines)
+            lines.append(next(reader))
 
-    col_spec = ["{} \t{}".format(columns[i], types[i]) for i in range(0, len(columns))]
+    if not rows:
+        raise Exception("No data in {}".format(file_path))
+    types = guess_types(rows, lines)
+    for row in rows:
+        for cell in row:
+            if ',' in cell:
+                has_commas = True
+                break
+
+    sql_columns = [c.replace('.', '_') for c in columns]
+    col_spec = ["{} \t{}".format(sql_columns[i], types[i]) for i in range(0, len(columns))]
     ddl = "CREATE TABLE {}\n ({})".format(name, ",\n\t".join(col_spec))
 
-    for c in columns:
-        if c.lower() in index_columns:
+    for c in sql_columns:
+        if is_index_column(c):
             ddl += ";\nCREATE INDEX {table}_{column}_idx ON {table} ({column})"\
                 .format(table = name, column = c)
 
@@ -201,14 +248,52 @@ def create_table(file_path: str, cursor = None) -> str:
 
     if cursor:
         cursor.execute(ddl)
-        with open(file_path) as data:
-            with CSVFileWrapper(data) as csv_data:
-                csv_data.readline()
-                cursor.copy_from(csv_data, name, sep=',', null="NA", columns=columns)
-
-
+        if has_commas:
+            copy_data(cursor, name, sql_columns, types, file_path)
+        else:
+            with open(file_path) as data:
+                with CSVFileWrapper(data) as csv_data:
+                    csv_data.readline()
+                    cursor.copy_from(csv_data, name, sep=',', null=NA, columns=sql_columns)
 
     return name
+
+
+def copy_data (cursor, table, columns, types, file_path):
+    N = 10000
+    insert = "INSERT INTO {table} ({columns}) VALUES "\
+        .format(table = table, columns=','.join(columns))
+    #values = "({})".format('')
+    with open(file_path) as data:
+        reader = csv.reader(data, quotechar='"', delimiter=',',
+                     quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        next(reader)
+        counter = 0
+        sql = insert
+        while True:
+            try:
+                row = next(reader)
+            except(StopIteration):
+                break
+            counter += 1
+            for i in range(0, len(types)):
+                if row[i] == NA:
+                    row[i] = "NULL"
+                elif types[i] == "VARCHAR":
+                    row[i] = "'{}'".format(row[i].replace("'", "''"))
+            values = "({})".format(','.join(row))
+            sql += values
+            if (counter % N) == 0:
+                cursor.execute(sql)
+                print(counter)
+                sql = insert
+            else:
+                sql += ","
+        if (counter % N) != 0:
+            if sql[-1] == ',':
+                sql = sql[:-1]
+            cursor.execute(sql)
+    return
 
 
 def ingest(path: str) -> str:
