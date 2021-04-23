68df065405f0d354f9c89424418fc2e27099fef0
misha <michael.bouzinier@forome.org>
2021-04-23 16:54:58 -0400

    MAB   Ingesting medicaid files

---
+++ src/python/nsaph/cms/medicaid.py
@@ -0,0 +1,15 @@
+'''
+https://github.com/NSAPH/data_model
+
+Demographics:   /data/incoming/rce/ci3_d_medicaid/processed_data/cms_medicaid-max/data_cms_medicaid-max-demographics_patient
+    Description of columns:
+        https://gitlab-int.rc.fas.harvard.edu/rse/francesca_dominici/dominici_data_pipelines/-/blob/master/medicaid/1_create_demographics_data.R
+Enrollments:    /data/incoming/rce/ci3_d_medicaid/processed_data/cms_medicaid-max/data_cms_medicaid-max-ps_patient-year
+    Description of columns:
+        https://gitlab-int.rc.fas.harvard.edu/rse/francesca_dominici/dominici_data_pipelines/-/blob/master/medicaid/2_process_enrollment_data.R
+        https://github.com/NSAPH/data_requests/tree/master/request_projects/dec2019_medicaid_platform_cvd
+Admissions:     /data/incoming/rce/ci3_health_data/medicaid/cvd/1999_2012/desouza/data
+    Description of columns:
+        https://gitlab-int.rc.fas.harvard.edu/rse/francesca_dominici/dominici_data_pipelines/-/blob/master/medicaid/code/2_create_cvd_data.R
+        https://github.com/NSAPH/data_requests/blob/master/request_projects/dec2019_medicaid_platform_cvd/cvd_readme.md
+'''
