0e586878a66bc9587a1303dcb93fb3ac5efd22a7
misha <michael.bouzinier@forome.org>
2021-05-04 03:50:36 -0400

    MAB   Implementing downstream medicaid enrollments pipeline

--- src/python/nsaph/model2.py
+++ src/python/nsaph/model2.py
@@ -5,11 +5,13 @@ import sys
 from pathlib import Path
 
 from nsaph_utils.utils.io_utils import as_dict, fopen
+from nsaph_utils.utils.pyfst import FSTReader
 from psycopg2.extras import execute_values
 
 from nsaph import init_logging
 from nsaph.db import Connection
 from nsaph.model import INDEX_DDL_PATTERN, INDEX_NAME_PATTERN, index_method
+from nsaph.fips import fips_dict
 
 
 def compute_column(column: dict) -> str:
@@ -40,7 +42,7 @@ class Domain:
         else:
             raise Exception("Invalid indexing policy: " + index_policy)
 
-    def generate_ddl(self) -> None:
+    def init(self) -> None:
         if self.schema:
             self.ddl = ["CREATE SCHEMA IF NOT EXISTS {};".format(self.schema)]
         else:
@@ -171,18 +173,32 @@ class Domain:
             raise Exception("Unsupported spec type for column: " + name)
         return name, column
 
+    @staticmethod
+    def is_generated(column):
+        if not isinstance(column, dict):
+            return False
+        if "source" not in column:
+            return False
+        if not isinstance(column["source"], dict):
+            return False
+        if "type" not in column["source"]:
+            return False
+        return "generated" == column["source"]["type"].lower()
+
     def column_spec(self, column) -> str:
         name, column = self.split(column)
         t = column.get("type", "VARCHAR")
-        if "source" in column and column["source"] == "generated":
+        if self.is_generated(column):
             if not "code" in column["source"]:
                 raise Exception("Generated column must specify the compute code")
             code = column["source"]["code"]
             return "{} {} {}".format(name, t, code)
         return "{} {}".format(name, t)
 
-    def map(self, columns, csv_columns) -> dict:
+    def map(self, columns, csv_columns) -> (dict, dict, dict):
         mapping = dict()
+        computes = dict()
+        child_inserts = dict()
         for c in columns:
             name, column = self.split(c)
             source = None
@@ -190,13 +206,16 @@ class Domain:
                 if isinstance(column["source"], str):
                     source = column["source"]
                 elif isinstance(column["source"], dict):
-                    t = column["source"]["name"]["type"]
+                    t = column["source"]["type"]
                     if t == "column":
                         source = column["source"]["column"]
                     elif t == "multi_column":
                         raise Exception("Not implemented: " + t)
                     elif t == "compute":
-                        raise Exception("Not implemented: " + t)
+                        computes[name] = column["source"]
+                        continue
+                    elif t == "generated":
+                        continue
                     else:
                         raise Exception("Invalid source for column {}: {}".format(name, str(column["source"])))
                 else:
@@ -210,16 +229,36 @@ class Domain:
                 raise Exception("Source was not found for column {}".format(name))
             source_index = csv_columns.index(source)
             mapping[source_index] = name
-        return mapping
+        inverse_mapping = {
+            item[1]: item[0] for item in mapping.items()
+        }
+        for compute in computes.values():
+            parameters = [
+                inverse_mapping[p] for p in compute["parameters"]
+            ]
+            arguments = ["empty"] + [
+                "row[{:d}]".format(p) for p in parameters
+            ]
+            code = compute["code"].format(*arguments)
+            compute["eval"] = code
+        return mapping, computes, child_inserts
 
-    def read(self, reader, mapping: dict, pk: set):
+    def compute(self, compute, row):
+        try:
+            value = eval(compute["eval"])
+        except:
+            value = None
+        return value
+
+    def read(self, reader, mapping: dict, computes: dict, pk: set):
         l = 1
         records = []
         for row in reader:
             record = []
             is_valid = True
             for i in sorted(mapping):
-                if not row[i]:
+                value = row[i]
+                if not value:
                     if i in pk:
                         logging.warning("Illegal row #{:d}: {}".format(l, str(row)))
                         is_valid = False
@@ -227,16 +266,19 @@ class Domain:
                     else:
                         record.append(None)
                 else:
-                    record.append(row[i])
+                    record.append(value)
             if not is_valid:
                 continue
+            for c in computes:
+                value = self.compute(computes[c], row)
+                record.append(value)
             records.append(record)
             l += 1
             if l > self.page_size:
                 break
         return records
 
-    def import_csv(self, node, path, cursor):
+    def import_file(self, node, path, cursor, limit = None, log_step = 1000000):
         if isinstance(node, str):
             table = node
             definition = self.spec[self.domain]["tables"][table]
@@ -244,24 +286,40 @@ class Domain:
             table, definition = node
         table = self.fqn(table)
         columns = definition["columns"]
-        with fopen(path, "r") as csv_file:
-            reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC)
+
+        if path.lower().endswith(".fst"):
+            reader = FSTReader(path)
+            reader.open()
+            to_close = reader
+            csv_columns = list(reader.columns.keys())
+        elif ".csv" in path.lower():
+            to_close = fopen(path, "r")
+            reader = csv.reader(to_close, quoting=csv.QUOTE_NONNUMERIC)
             header = next(reader)
             csv_columns = header
-            mapping = self.map(columns, csv_columns)
-            column_list = ", ".join([mapping[i] for i in sorted(mapping)])
+        else:
+            raise Exception("Unsupported file format: " + path)
+        try:
+            mapping, computes, child_inserts = self.map(columns, csv_columns)
+            cc = [mapping[i] for i in sorted(mapping)]
+            cc.extend(computes.keys())
+            column_list = ", ".join(cc)
             sql = "INSERT INTO {table} ({columns})  VALUES %s".format(table=table, columns=column_list)
             l = 0
             pk = {i for i in mapping if mapping[i] in definition["primary_key"]}
             while True:
-                records = self.read(reader, mapping, pk)
+                records = self.read(reader, mapping, computes, pk)
                 l += len(records)
                 if records:
                     execute_values(cursor, sql, records, page_size=self.page_size)
                 if len(records) < self.page_size:
                     break
-                if (l % 1000000) == 0:
+                if (l % log_step) == 0:
                     logging.info("Records imported: {:d}".format(l))
+                if limit and l > limit:
+                    break
+        finally:
+            to_close.close()
         return
 
     def create_tables(self, cursor):
@@ -273,7 +331,7 @@ class Domain:
 
 
 
-def test(argv):
+def test_init(argv):
     if len(argv) > 1:
         path_to_csv = argv[1]
     else:
@@ -281,16 +339,42 @@ def test(argv):
     src = Path(__file__).parents[2]
     registry_path = os.path.join(src, "yml", "medicaid.yaml")
     domain = Domain(registry_path, "medicaid")
-    domain.generate_ddl()
+    domain.init()
     for index in domain.indices:
         print(index)
     with Connection("database.ini", "nsaph2") as connection:
         cursor = connection.cursor()
         domain.create_tables(cursor)
-        domain.import_csv("demographics", path_to_csv, cursor)
+        domain.import_file("demographics", path_to_csv, cursor)
         connection.commit()
+
+
+def test_step2(argv):
+    if len(argv) > 1:
+        path_to_fst = argv[1]
+    else:
+        path_to_fst = "data/medicaid/medicaid_mortality_2001.fst"
+    src = Path(__file__).parents[2]
+    registry_path = os.path.join(src, "yml", "medicaid.yaml")
+    domain = Domain(registry_path, "medicaid")
+    domain.init()
+    node = ("enrollments_year", domain.spec[domain.domain]["tables"]["demographics"]["children"]["enrollments_year"])
+    with Connection("database.ini", "nsaph2") as connection:
+        cursor = connection.cursor()
+        domain.import_file(node, path_to_fst, cursor, limit=100000, log_step=10000)
+        connection.commit()
+
+def print_ddl():
+    src = Path(__file__).parents[2]
+    registry_path = os.path.join(src, "yml", "medicaid.yaml")
+    domain = Domain(registry_path, "medicaid")
+    domain.init()
+    for ddl in domain.ddl:
+        print(ddl)
+
 
 
 if __name__ == '__main__':
     init_logging()
-    test(sys.argv)
+    test_step2(sys.argv)
+    #print_ddl()
