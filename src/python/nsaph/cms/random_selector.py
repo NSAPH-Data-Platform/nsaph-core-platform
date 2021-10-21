363f64ea41ee8dc697db223faf50fccd78af8762
mmcentre <michael.bouzinier@kvitneset.com>
2021-10-21 13:56:04 -0400

    MAB     Random selector for CMS data

---
+++ src/python/nsaph/cms/random_selector.py
@@ -0,0 +1,37 @@
+import gzip
+import os
+import glob
+import random
+
+from nsaph_utils.utils.io_utils import fopen
+
+
+SEED = 1
+
+
+def select(pattern: str, destination: str, threshold: float):
+    files = glob.glob(pattern)
+    random.seed(SEED)
+    for f in files:
+        name = os.path.basename(f)
+        if not name.endswith('.gz'):
+            name += ".gz"
+        dest = os.path.basename(os.path.dirname(f))
+        dest = os.path.join(destination, dest, name)
+        print("{} ==> {}".format(f, dest))
+
+        with fopen(f, "rt") as src, gzip.open(dest, "wt") as output:
+            n1 = 0
+            n2 = 0
+            for line in src:
+                n1 += 1
+                if random.random() < threshold:
+                    output.write(line)
+                    n2 += 1
+        print("{:d}/{:d}".format(n2, n1))
+    print("All Done")
+
+
+if __name__ == '__main__':
+    select("*/*.csv*", "random_data", 0.02)
+
