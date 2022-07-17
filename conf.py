96560982edf5bde4c95446bd03ec87db4095060e
Mikhail Polykovskiy <mikhail.polykovskiy@quantori.com>
2022-07-17 19:59:21 +0400

    Local parser

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx_paramlinks',
     'sphinx.ext.autosectionlabel',
-    'recommonmark',
+    'nsaph_utils.docutils.recommonmark',
     'sphinx_markdown_tables'
 ]
 
