e879db1e25e610ad880ea35408dbf7547278cfc3
mmcentre <michael.bouzinier@kvitneset.com>
2022-10-06 11:10:04 -0400

    MAB      Include MedPar 2015 in the processing pipeline

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='nsaph',
-    version="0.0.3.11",
+    version="0.0.3.12",
     url='https://gitlab-int.rc.fas.harvard.edu/rse/francesca_dominici/nsaph',
     license='',
     author='Michael Bouzinier',
